9b48c09224492531a8465fd9190ece084b223f44
govindsingh004 <32063505+govindsingh004@users.noreply.github.com>
2017-09-18 17:24:59 +0530

    Add files via upload

---
+++ sgs sunecon2/excep-che/src/com/sgs/Test.java
@@ -0,0 +1,13 @@
+package com.sgs;
+
+public class Test {
+
+	public static void main(String[] args) throws ArithmeticException
+	
+	{
+		System.out.println(10/0);
+		// TODO Auto-generated method stub
+
+	}
+
+}
